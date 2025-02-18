eb3aaeb4548f8ff354ba956ac1ab69737073265d
Chun-Hsiang Yu <kihifung@gmail.com>
2025-02-18 16:37:33 +0800

    Lifting state into a parent component

--- src/App.js
+++ src/App.js
@@ -3,38 +3,42 @@ import { useState } from "react";
 
 console.log("Loading Square component");
 
-function Square() {
+function Square({ value, onSquareClick }) {
   const [value, setValue] = useState(null);
 
-  function handleClick() {
-    setValue("X");
-    console.log("clicked!");
-  }
-
   return (
-    <button className="square" onClick={handleClick}>
+    <button className="square" onClick={onSquareClick}>
       {value}
     </button>
   );
 }
 
 export default function Board() {
+  const [square, setSquare] = useState(Array(9).fill(null));
+
+  function handleClick(i) {
+    const nextSquares = squares.slice();
+    nextSquares[i] = "X";
+    setSquares(nextSquares);
+    console.log("clicked!");
+  }
+
   return (
     <>
       <div className="board-row">
-        <Square />
-        <Square />
-        <Square />
+        <Square value={square[0]} onSquareClick={() => handleClick(0)} />
+        <Square value={square[1]} onSquareClick={() => handleClick(1)} />
+        <Square value={square[2]} onSquareClick={() => handleClick(2)} />
       </div>
       <div className="board-row">
-        <Square />
-        <Square />
-        <Square />
+        <Square value={square[3]} onSquareClick={() => handleClick(3)} />
+        <Square value={square[4]} onSquareClick={() => handleClick(4)} />
+        <Square value={square[5]} onSquareClick={() => handleClick(5)} />
       </div>
       <div className="board-row">
-        <Square />
-        <Square />
-        <Square />
+        <Square value={square[6]} onSquareClick={() => handleClick(6)} />
+        <Square value={square[7]} onSquareClick={() => handleClick(7)} />
+        <Square value={square[8]} onSquareClick={() => handleClick(8)} />
       </div>
     </>
   );
