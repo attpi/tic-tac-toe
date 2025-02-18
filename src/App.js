139bf24bd4bdc59e659102ec257fd4c9908a920b
Chun-Hsiang Yu <kihifung@gmail.com>
2025-02-18 16:53:33 +0800

    add Taking turns: X and O

--- src/App.js
+++ src/App.js
@@ -14,12 +14,23 @@ function Square({ value, onSquareClick }) {
 }
 
 export default function Board() {
-  const [square, setSquare] = useState(Array(9).fill(null));
+  const [xIsNext, setXIsNext] = useState(true);
+  const [square, setSquares] = useState(Array(9).fill(null));
 
   function handleClick(i) {
+    if (squares[i]) {
+      return;
+    }
     const nextSquares = squares.slice();
+    if (xIsNext) {
+      nextSquares[i] = "X";
+    } else {
+      nextSquares[i] = "O";
+    }
+
     nextSquares[i] = "X";
     setSquares(nextSquares);
+    setXIsNext(!xIsNext);
     console.log("clicked!");
   }
 
